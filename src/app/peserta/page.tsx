b28950e0d3a184223d237e906d9b801ad1832be1
ARoyyanF <160403210+ARoyyanF@users.noreply.github.com>
2024-08-18 12:33:57 +0700

    sop ganti password & reset password

--- src/app/peserta/page.tsx
+++ src/app/peserta/page.tsx
@@ -9,66 +9,82 @@ import { News } from "../_components/news";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function PesertaPage() {
-    const session = await getServerAuthSession()
-    if (!session) {
-        redirect('/authpage/login/')
-    }
-    if (session.user.role === 'ADMIN') {
-        redirect('/')
-    }
+  const session = await getServerAuthSession();
+  if (!session) {
+    redirect("/authpage/login/");
+  }
+  if (session.user.role === "ADMIN") {
+    redirect("/");
+  }
 
-    const getCumulativeScore = await api.user.getCumulativeScore({ userId: session.user.id })
+  const getCumulativeScore = await api.user.getCumulativeScore({
+    userId: session.user.id,
+  });
 
-    return (
-        <div className="flex min-h-screen flex-col text-white bg-cover
-            bg-[url('/background.png')]"
-            >
-        
-            <div className="absolute top-0 right-0 max-w-[25rem] max-h-[15rem] bg-local"  // session status bar
-                        style={{
-                            backgroundImage: `url('/Papan-ungu.png')`,
-                            backgroundRepeat: "no-repeat",
-                            backgroundSize: "100% 100%"
-                        }}>
-                <div className="flex h-full flex-col items-center justify-center gap-2 pt-20 pb-12">
-                    <p className="text-center text-purple-50 font-extrabold px-20 font-bluecashews" >
-                    {session && <span>{session.user?.name}<br /> ({session.user?.role})</span>}<br />
-                        Points: {getCumulativeScore}
-                    </p>
-                    
-                    <Link
-                        href={"/api/auth/signout"}
-                            className="rounded bg-purple-900/80 text-purple-200 px-10 font-semibold no-underline transition hover:bg-white/20 font-roman"
-                        >Signout
-                    </Link>
-                    
-                </div>
-            </div>
-            <div className="flex min-h-screen flex-col items-center justify-center pt-60">
-                <h1 className="text-3xl sm:text-5xl font-extrabold tracking-tight md:text-6xl font-bluecashews">
-                    Intellektuelle<span className="text-[hsl(280,36%,51%)]">schule</span>
-                </h1>
-                <p className="pt-10 text-base w-9/12 text-center text-[hsl(280,100%,94%)] font-extrabold tracking-widest md:text-xl">
-                    Selamat datang, para kurcaci pengembara cahaya. Hutan yang lebat mungkin telah menutup pandanganmu, namun penduduk istana selalu menunggumu untuk menemuinya, tapi kapankah datangnya kesempatan itu? Jalanilah proses ini, hingga kau menemukan celah terang...
-                </p>
-                {/* <PostList/> */}
-                <div className="pt-24">
-                    <News/>
-                </div>
-                <div className="pt-24">
-                    <TugasListPeserta/>
-                </div>
-                <div className="flex flex-col items-center justify-center pt-10 pb-20">
-                    <p className=" text-base text-center text-[#772da8] font-extrabold tracking-widest md:text-2xl font-bluecashews">
-                        Teman adalah saudara <br/>
-                        Saudara adalah keluarga <br/>
-                        Kebersamaan adalah segalanya <br/>
-                    </p>
-                </div>
-                
-            </div>
+  return (
+    <div className="flex min-h-screen flex-col bg-[url('/background.png')] bg-cover text-white">
+      <div
+        className="absolute right-0 top-0 max-h-[20rem] max-w-[25rem] bg-local" // session status bar
+        style={{
+          backgroundImage: `url('/Papan-ungu.png')`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "100% 100%",
+        }}
+      >
+        <div className="flex h-full flex-col items-center justify-center gap-2 pb-12 pt-20">
+          <p className="px-20 text-center font-bluecashews font-extrabold text-purple-50">
+            {session && (
+              <span>
+                {session.user?.name}
+                <br /> ({session.user?.role})
+              </span>
+            )}
+            <br />
+            Points: {getCumulativeScore}
+          </p>
+
+          <Link
+            href={"/authpage/resetpassword"}
+            className="rounded bg-purple-900/80 px-10 font-roman font-semibold text-purple-200 no-underline transition hover:bg-white/20"
+          >
+            Ganti password
+          </Link>
+
+          <Link
+            href={"/api/auth/signout"}
+            className="rounded bg-purple-900/80 px-10 font-roman font-semibold text-purple-200 no-underline transition hover:bg-white/20"
+          >
+            Signout
+          </Link>
         </div>
-    );
+      </div>
+      <div className="flex min-h-screen flex-col items-center justify-center pt-60">
+        <h1 className="font-bluecashews text-3xl font-extrabold tracking-tight sm:text-5xl md:text-6xl">
+          Intellektuelle<span className="text-[hsl(280,36%,51%)]">schule</span>
+        </h1>
+        <p className="w-9/12 pt-10 text-center text-base font-extrabold tracking-widest text-[hsl(280,100%,94%)] md:text-xl">
+          Selamat datang, para kurcaci pengembara cahaya. Hutan yang lebat
+          mungkin telah menutup pandanganmu, namun penduduk istana selalu
+          menunggumu untuk menemuinya, tapi kapankah datangnya kesempatan itu?
+          Jalanilah proses ini, hingga kau menemukan celah terang...
+        </p>
+        {/* <PostList/> */}
+        <div className="pt-24">
+          <News />
+        </div>
+        <div className="pt-24">
+          <TugasListPeserta />
+        </div>
+        <div className="flex flex-col items-center justify-center pb-20 pt-10">
+          <p className="text-center font-bluecashews text-base font-extrabold tracking-widest text-[#772da8] md:text-2xl">
+            Teman adalah saudara <br />
+            Saudara adalah keluarga <br />
+            Kebersamaan adalah segalanya <br />
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 // todo:
