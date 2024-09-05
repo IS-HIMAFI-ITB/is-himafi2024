da95546061775d54b67ce7f240f08d74cbc777f7
ARoyyanF <160403210+ARoyyanF@users.noreply.github.com>
2024-09-05 23:32:43 +0700

    display score on scored tugases

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,38 @@
 import "~/styles/globals.css";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
-import localfont from "next/font/local"
+import localfont from "next/font/local";
 
 import { TRPCReactProvider } from "~/trpc/react";
 
 const bluecashews = localfont({
-  src: [{
-    path:'../../public/fonts/Blue_Cashews.otf',
-  }],
+  src: [
+    {
+      path: "../../public/fonts/Blue_Cashews.otf",
+    },
+  ],
   variable: "--font-bluecashews",
-})
+});
 
 const roman = localfont({
-  src: [{
-    path:'../../public/fonts/RomanSerif.ttf',
-  }],
+  src: [
+    {
+      path: "../../public/fonts/RomanSerif.ttf",
+    },
+  ],
   variable: "--font-roman",
-})
+});
+
+const bBayanganPendekar = localfont({
+  src: [
+    {
+      path: "../../public/fonts/bBayanganPendekar.ttf",
+    },
+  ],
+  variable: "--font-bBayanganPendekar",
+});
 
 export const metadata: Metadata = {
   title: "Intellektuelle Schule 2024",
@@ -27,11 +40,9 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable} ${bluecashews.variable} ${roman.variable} font-roman`}>
+    <html lang="en" className={`${GeistSans.variable} ${bluecashews.variable} ${roman.variable} ${bBayanganPendekar.variable} font-roman `}>
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
         <Toaster />
