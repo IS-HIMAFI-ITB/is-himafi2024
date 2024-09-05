973b055b68c616888e6b922c528daf3fb7a10fb1
ARoyyanF <160403210+ARoyyanF@users.noreply.github.com>
2024-09-05 21:20:30 +0700

    lowered dbParallel connection limit to 60 to minimize possibility of max_connection_limit_reached error

--- src/server/api/routers/sheetsCMSTugas.ts
+++ src/server/api/routers/sheetsCMSTugas.ts
@@ -16,7 +16,7 @@ import { Prisma } from "@prisma/client";
 const dbParallel = new PrismaClient({
   datasources: {
     db: {
-      url: process.env.DATABASE_URL_NOPARAM + "?pgbouncer=true&connection_limit=100&pool_timeout=60",
+      url: process.env.DATABASE_URL_NOPARAM + "?pgbouncer=true&connection_limit=60&pool_timeout=60",
     },
   },
   log: ["error"],
