bfe2c7565ef9223e563ece843fb96933e373ee6b
ARoyyanF <160403210+ARoyyanF@users.noreply.github.com>
2024-09-19 07:38:45 +0700

    content: remove titik koordinate style: refined countdown placement

--- src/app/_components/countdown.tsx
+++ src/app/_components/countdown.tsx
@@ -31,19 +31,18 @@ export default function Countdown({ count }: { count: Date }) {
   }, []);
 
   return (
-    <div className="w-screen px-10 sm:px-[15%]">
+    <div className="w-screen px-10 md:px-[20%] sm:px-[10%]">
       <div className="text-center content-stretch">
         <h1 className="text-5xl md:text-8xl font-bold mb-8 text-[#39a2e2] animate-pulse w-auto">Hari Kemenangan</h1>
-        <div className="grid grid-cols-3 sm:gap-4">
+        <div className="grid grid-cols-3 sm:gap-10">
           {Object.entries(timeLeft).map(([unit, value]) => (
-            <div key={unit} className="bg-gray-800/50 p-4 rounded-lg shadow-lg">
+            <div key={unit} className="bg-gray-800/50 sm:p-4 py-4 rounded-lg shadow-lg">
               <div className="text-5xl md:text-6xl font-bold text-gray-100 mb-2">{value.toString().padStart(2, "0")}</div>
-              <div className="text-xs sm:text-xl text-gray-400 uppercase overflow-clip">{unit}</div>
+              <div className="text-sm sm:text-xl text-gray-400 uppercase overflow-clip">{unit}</div>
             </div>
           ))}
         </div>
         <div className="my-20 text-2xl md:text-4xl gap-5 grid grid-cols-1 text-gray-300 whitespace-pre-wrap text-center ">
-          <div>{`Titik koordinat:`}</div>
           <div className="font-black font-mono tracking-widest overflow-x-auto no-scrollbar">{`-6.8643391, 107.6178311`}</div>
           <div>{`Tidak ada keluarga yang ditinggalkan.`}</div>
         </div>
